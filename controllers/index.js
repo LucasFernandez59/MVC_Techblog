bca875b7318ba4390e8d2e5fa201feec95fc1ef6
LucasF <che.fernandez255@gmail.com>
2023-11-25 20:39:03 -0700

    established server and routes

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+// Import the necessary modules and routes
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes");
+
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
+
+module.exports = router;
