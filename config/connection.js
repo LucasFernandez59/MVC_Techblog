aa7a41694b72bad7f1b4d4583409b49f1b0a34a4
LucasF <che.fernandez255@gmail.com>
2023-11-25 21:35:59 -0700

    mas testing

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const sequelize = process.env.JAWSDB_URL 
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: '0.0.0.0',
+      host: process.env.DB_HOST,
       dialect: 'mysql',
-      port: 3000
+      port: process.env.DB_PORT
       // dialectOptions: {
       //   decimalNumbers: true,
       // },
