09c834594f110132cd481cc6d6aac787213d8223
LucasF <che.fernandez255@gmail.com>
2023-11-25 22:23:08 -0700

    testamundo

--- config/connection.js
+++ config/connection.js
@@ -22,7 +22,7 @@ if (process.env.MYSQL_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: '127.0.0.1',
+      host: '0.0.0.0',
       dialect: 'mysql',
       port: 3306
     }
