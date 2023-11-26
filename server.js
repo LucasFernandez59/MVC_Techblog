22d0054c5ca4166a8b698da5c9e733e684003a39
LucasF <che.fernandez255@gmail.com>
2023-11-25 21:57:50 -0700

    mucho testo

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const session = require("express-session");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ helpers: require("./utils/helpers") });
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -34,7 +36,7 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 
-app.use(session(sess));
+// app.use(session(sess));
 //   session({
 //     secret: process.env.SECRET,
 //     store: new SequelizeStore({ db: sequelize }),
