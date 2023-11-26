b73a580f3b0ad09b2aaf70c7e29749fcb3ffc08b
LucasF <che.fernandez255@gmail.com>
2023-11-25 21:30:43 -0700

    even more testing

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: '0.0.0.0',
       dialect: 'mysql',
-      port: 3306
+      port: 3000
       // dialectOptions: {
       //   decimalNumbers: true,
       // },
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ helpers: require("./utils/helpers") });
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const sess = {
   secret: 'Super secret secret',
   cookie: {
